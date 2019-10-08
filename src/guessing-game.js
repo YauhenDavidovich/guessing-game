9cc165495ad5754ecd8b520f859a1466f958a7e4
Евгений Давидович <davidovichyauhen@gmail.com>
2019-10-08 23:17:28 +0300

    feat: this is solution for task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        
     }
 
     guess() {
+        this.guess_number = Math.ceil((Math.floor (this.min + this.max  ) / 2)); //find the average and round to the big side
+        return this.guess_number;
+        
+       
 
     }
 
     lower() {
+        this.max = this.guess_number;
+        return this.guess_number;
+
 
     }
 
     greater() {
+        this.min = this.guess_number;
+        return this.guess_number;
 
     }
 }
-
 module.exports = GuessingGame;
